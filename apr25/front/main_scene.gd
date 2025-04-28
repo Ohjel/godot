02b101c927f800168bad40ce5813ddd3621ab3cf
Maxime Aerts <maxime.aerts@dermatoo.com>
2025-04-28 23:39:44 +0200

    Add maze tilemap

--- apr25/front/main_scene.gd
+++ apr25/front/main_scene.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-var time_elapsed: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $Control/Label.modulate = Color.GREEN_YELLOW
+    pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    time_elapsed += delta
-    $Control/Label.text = 'Time elapsed: ' + str(roundf(time_elapsed)) + 's'
+func _process(_delta):
+    pass
